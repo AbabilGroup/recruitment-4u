721d2b9d9c10a8e6569b939d50d0d116bf07ae42
jihansoft <jihanuddinsoft@gmail.com>
2025-04-15 13:40:10 +0600

    error fix final recruitment page

--- src/components/pages-layout/Recruitment/Whatsdo.tsx
+++ src/components/pages-layout/Recruitment/Whatsdo.tsx
@@ -8,54 +8,56 @@ const cardVariants = {
 };
 
 const Whatsdo = () => {
-   <section className="bg-[#009AEE] py-12">
-     <div className="container">
-       <motion.div className="flex justify-center items-center max-md:flex-col gap-4 md:gap-8">
-         <motion.div
-           className="w-full sm:w-[360px] cursor-pointer"
-           variants={cardVariants}>
-           <Card
-             title="Recruitment"
-             description="We provide full-cycle recruiting & HR services that are tailored to the business needs."
-             avatars={[
-               "https://randomuser.me/api/portraits/women/1.jpg",
-               "https://randomuser.me/api/portraits/women/2.jpg",
-             ]}
-             bgColor="bg-[#05123F]"
-           />
-         </motion.div>
+  return (
+    <section className="bg-[#009AEE] py-12">
+      <div className="container">
+        <motion.div className="flex justify-center items-center max-md:flex-col gap-4 md:gap-8">
+          <motion.div
+            className="w-full sm:w-[360px] cursor-pointer"
+            variants={cardVariants}>
+            <Card
+              title="Recruitment"
+              description="We provide full-cycle recruiting & HR services that are tailored to the business needs."
+              avatars={[
+                "https://randomuser.me/api/portraits/women/1.jpg",
+                "https://randomuser.me/api/portraits/women/2.jpg",
+              ]}
+              bgColor="bg-[#05123F]"
+            />
+          </motion.div>
 
-         <motion.div
-           className="w-full sm:w-[360px] cursor-pointer"
-           variants={cardVariants}>
-           <Card
-             title="HR-consulting"
-             description="We help companies create a well-known brand that will stand out on the market and boost hiring metrics."
-             avatars={[
-               "https://randomuser.me/api/portraits/women/1.jpg",
-               "https://randomuser.me/api/portraits/women/2.jpg",
-             ]}
-             bgColor="bg-[#FFFFFF]"
-           />
-         </motion.div>
+          <motion.div
+            className="w-full sm:w-[360px] cursor-pointer"
+            variants={cardVariants}>
+            <Card
+              title="HR-consulting"
+              description="We help companies create a well-known brand that will stand out on the market and boost hiring metrics."
+              avatars={[
+                "https://randomuser.me/api/portraits/women/1.jpg",
+                "https://randomuser.me/api/portraits/women/2.jpg",
+              ]}
+              bgColor="bg-[#FFFFFF]"
+            />
+          </motion.div>
 
-         <motion.div
-           // onClick={() => handleButtonClick("/outstaffing")}
-           className="w-full sm:w-[360px] cursor-pointer"
-           variants={cardVariants}>
-           <Card
-             title="Outstaffing"
-             description="We provide outstaffing for fast-growing companies, help to grow companies' teams and increase their productivity"
-             avatars={[
-               "https://randomuser.me/api/portraits/women/1.jpg",
-               "https://randomuser.me/api/portraits/women/2.jpg",
-             ]}
-             bgColor="bg-[#90DA7B]"
-           />
-         </motion.div>
-       </motion.div>
-     </div>
-   </section>;
-}
+          <motion.div
+            // onClick={() => handleButtonClick("/outstaffing")}
+            className="w-full sm:w-[360px] cursor-pointer"
+            variants={cardVariants}>
+            <Card
+              title="Outstaffing"
+              description="We provide outstaffing for fast-growing companies, help to grow companies' teams and increase their productivity"
+              avatars={[
+                "https://randomuser.me/api/portraits/women/1.jpg",
+                "https://randomuser.me/api/portraits/women/2.jpg",
+              ]}
+              bgColor="bg-[#90DA7B]"
+            />
+          </motion.div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
-export default Whatsdo
+export default Whatsdo;
